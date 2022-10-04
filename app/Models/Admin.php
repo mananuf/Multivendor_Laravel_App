5f8e675c1e2711008e6de158683dcd5f9e4f1c69
mananuf <bmananaf@gmail.com>
2022-10-04 16:42:53 +0100

    adds fillable to model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'type',
+        'vendor_id',
+        'phone',
+        'email',
+        'password',
+        'image',
+        'status'
+    ];
 }
