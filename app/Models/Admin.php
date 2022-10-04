f7ae55be56161cf66850fd4efe6733b241f38b01
mananuf <bmananaf@gmail.com>
2022-10-04 17:00:14 +0100

    adds admin guard

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth\User as Authenticatable;
 
 class Admin extends Model
 {
     use HasFactory;
+    // guard
+    protected $guard = 'admin';
     protected $fillable = [
         'name',
         'type',
