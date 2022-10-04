1b2c3e889b3b0bff438e14c7a6bfd6b5203edfa6
mananuf <bmananaf@gmail.com>
2022-10-04 15:45:42 +0100

    modifies template

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,10 +1,32 @@
-@extends('admin.layouts.layout')
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>{{ config('app.name') }}</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="{{ asset('admin/vendors/feather/feather.css') }}">
+  <link rel="stylesheet" href="{{ asset('admin/vendors/ti-icons/css/themify-icons.css')}}">
+  <link rel="stylesheet" href="{{ asset('admin/vendors/css/vendor.bundle.base.css') }}">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="{{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+  <link rel="stylesheet" href="{{ asset('admin/vendors/ti-icons/css/themify-icons.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('admin/js/select.dataTables.min.css') }}">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="{{ asset('admin/css/vertical-layout-light/style.css') }}">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="{{ asset('admin/images/favicon.png') }}" />
+</head>
+<body>
 <div class="container-scroller">
     <div class="container-fluid page-body-wrapper full-page-wrapper">
       <div class="content-wrapper d-flex align-items-center auth px-0">
         <div class="row w-100 mx-0">
-          <div class="col-lg-4 mx-auto">
+          <div class="col-lg-6 mx-auto">
             <div class="auth-form-light text-left py-5 px-4 px-sm-5">
               <div class="brand-logo">
                 <img src="{{asset('admin/images/logo.svg')}}" alt="logo">
@@ -47,4 +69,29 @@
     </div>
     <!-- page-body-wrapper ends -->
   </div>
-@endsection
+
+<!-- plugins:js -->
+<script src="{{asset('admin/vendors/js/vendor.bundle.base.js') }}"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<script src="{{asset('admin/vendors/chart.js/Chart.min.js') }}"></script>
+<script src="{{asset('admin/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+<script src="{{asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+<script src="{{asset('admin/js/dataTables.select.min.js') }}"></script>
+
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="{{asset('admin/js/off-canvas.js') }}"></script>
+<script src="{{asset('admin/js/hoverable-collapse.js') }}"></script>
+<script src="{{asset('admin/js/template.js') }}"></script>
+<script src="{{asset('admin/js/settings.js') }}"></script>
+<script src="{{asset('admin/js/todolist.js') }}"></script>
+<!-- endinject -->
+<!-- Custom js for this page-->
+<script src="{{asset('admin/js/dashboard.js')}}"></script>
+<script src="{{asset('admin/js/Chart.roundedBarCharts.js')}}"></script>
+<!-- End custom js for this page-->
+</body>
+
+</html>
+
