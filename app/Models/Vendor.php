c0d16fe59b44893fd288c4dca91abc4e0c98a40d
mananuf <bmananaf@gmail.com>
2022-10-04 16:34:16 +0100

    creates Vendor model and adds fillable

---
+++ app/Models/Vendor.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vendor extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'address',
+        'city',
+        'state',
+        'country',
+        'pincode',
+        'phone',
+        'email',
+        'status'
+    ];
+}
