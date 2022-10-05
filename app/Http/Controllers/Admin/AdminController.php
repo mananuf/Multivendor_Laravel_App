0a8fe51d32a61b493c985ab963a746086aec5ca2
mananuf <bmananaf@gmail.com>
2022-10-05 02:18:25 +0100

    adds admin login logic

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
@@ -14,8 +15,30 @@ class AdminController extends Controller
         return view('admin.dashboard');
     }
 
-    public function login()
+    public function login(Request $request)
     {
+        if ($request->method('POST')) {
+            // validate form
+            $request->validate([
+                'email' => 'required',
+                'password' => 'required'
+            ]);
+            // get form data
+            $form_data = $request->all();
+            // get credentials
+            $credentials = [
+                'email' => $form_data['email'],
+                'password' => $form_data['password'],
+                'status' => 1
+            ];
+
+            // if credentials match
+            if (Auth::guard('admin')->attempt($credentials)) {
+                return redirect('/admin/dashboard');
+            } else {
+                return redirect()->back()->with('error', 'invalid credentials, try again');
+            }
+        }
         // echo Hash::make('superpass');
         return view('admin.login');
     }
