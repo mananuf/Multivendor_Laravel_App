0c3aee4dd563627c542f94f658d7c2de3c8d31b0
mananuf <bmananaf@gmail.com>
2022-10-05 02:25:19 +0100

    addds form action and method

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -33,7 +33,9 @@
               </div>
               <h4>Hello! let's get started</h4>
               <h6 class="font-weight-light">Sign in to continue.</h6>
-              <form class="pt-3">
+              {{-- form start --}}
+              <form class="pt-3" method="POST" action="{{route('admin.login')}}">
+                @csrf
                 <div class="form-group">
                   <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username">
                 </div>
@@ -41,7 +43,7 @@
                   <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
                 </div>
                 <div class="mt-3">
-                  <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">SIGN IN</a>
+                  <button type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" >SIGN IN</button>
                 </div>
                 <div class="my-2 d-flex justify-content-between align-items-center">
                   <div class="form-check">
