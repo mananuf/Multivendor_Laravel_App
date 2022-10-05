8c79eed2806d7915ba7101d415af6f16dcc1781e
mananuf <bmananaf@gmail.com>
2022-10-05 03:53:50 +0100

    adds messages components

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -26,6 +26,9 @@
     <div class="container-fluid page-body-wrapper full-page-wrapper">
       <div class="content-wrapper d-flex align-items-center auth px-0">
         <div class="row w-100 mx-0">
+          <div class="d-block">
+            <x-messages/>
+          </div>
           <div class="col-lg-6 mx-auto">
             <div class="auth-form-light text-left py-5 px-4 px-sm-5">
               <div class="brand-logo">
