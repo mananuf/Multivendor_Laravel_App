361e2d99168439147726418c5f7656ca646e164a
mananuf <bmananaf@gmail.com>
2022-10-05 03:19:30 +0100

    extends Authenticable class

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+
+class Admin extends Authenticatable
 {
     use HasFactory;
     // guard
