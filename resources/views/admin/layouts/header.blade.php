1226601ec90d99f90101b152360a707344f53857
mananuf <bmananaf@gmail.com>
2022-11-04 23:55:00 +0100

    displays profile picture

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -72,8 +72,9 @@
           </div>
         </li>
         <li class="nav-item nav-profile dropdown">
+          {{-- {{dd(asset('storage/admin/'.Auth::guard('admin')->user()->image))}} --}}
           <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
-            <img src="{{asset('admin/images/faces/face28.jpg') }}" alt="profile"/>
+            <img src="{{Auth::guard('admin')->user()->image ? asset('storage/admin/'.Auth::guard('admin')->user()->image): asset('img/default_user.png') }}" alt="profile"/>
           </a>
           <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
             <a class="dropdown-item">
