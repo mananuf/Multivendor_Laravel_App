076d910d4c1c9e7e742cf604b5a375d12131d8a5
mananuf <bmananaf@gmail.com>
2022-11-04 17:05:34 +0100

    modifies form

--- resources/views/admin/settings/update-admin-password.blade.php
+++ resources/views/admin/settings/update-admin-password.blade.php
@@ -2,12 +2,14 @@
 @section('content')
 <div class="col-md-6 grid-margin stretch-card">
               <div class="card">
+                <x-messages/>
                 <div class="card-body">
                   <h4 class="card-title">Update Details</h4>
                   <p class="card-description">
                     {{Auth::guard('admin')->user()->name}}
                   </p>
                   <form class="forms-sample" method="POST" action="{{route('admin.password.update')}}">
+                    @csrf
                     <div class="form-group">
                       <label for="exampleInputUsername1">Username</label>
                       <input class="form-control" id="exampleInputUsername1" value="{{Auth::guard('admin')->user()->name}}" readonly>
@@ -16,10 +18,7 @@
                       <label for="exampleInputEmail1">Email address</label>
                       <input class="form-control" id="exampleInputEmail1" placeholder="Email" value="{{Auth::guard('admin')->user()->email}}" readonly>
                     </div>
-                    <div class="form-group">
-                      <label for="current_password">Current Password</label>
-                      <input type="password" class="form-control" id="current_password" placeholder="Current Password" name="current_password">
-                    </div>
+                    
                     <div class="form-group">
                         <label for="new_password">New Password</label>
                         <input type="password" class="form-control" id="new_password" placeholder="New Password" name="new_password">
@@ -29,6 +28,7 @@
                       <input type="password" class="form-control" id="password_confirmation" placeholder="Confirm Password" name="password_confirmation">
                     </div>
                     <button type="submit" class="btn btn-primary mr-2">Update</button>
+
                     <a class="btn btn-light" href="{{route('admin.dashboard')}}">Cancel</a>
                   </form>
                 </div>
