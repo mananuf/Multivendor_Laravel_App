7089653273b5509024f5b5932373908e8658f797
mananuf <bmananaf@gmail.com>
2022-11-16 17:25:35 +0100

    adds vendorBankDetail and vendorBusinessDetail seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,7 +22,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(AdminTableSeeder::class);
-        $this->call(VendorsTableSeeder::class);
+        // $this->call(AdminTableSeeder::class);
+        // $this->call(VendorsTableSeeder::class);
+        $this->call(VendorsBusinessDetailTableSeeder::class);
+        $this->call(VendorBankDetailTableSeeder::class);
     }
 }
