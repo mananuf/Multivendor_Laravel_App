743944413fbe154422f996d6e0b105148617017f
mananuf <bmananaf@gmail.com>
2022-11-18 23:34:39 +0100

    creates section method

--- app/Http/Controllers/Admin/SectionController.php
+++ app/Http/Controllers/Admin/SectionController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Section;
 use Illuminate\Http\Request;
 
 class SectionController extends Controller
 {
     //
+    public function sections()
+    {
+        $sections = Section::get();
+        $title = 'Sections';
+        $description = 'A list of all sections in this app';
+        return view(
+            'admin.sections.sections',
+            compact(
+                'sections',
+                'title',
+                'description'
+            )
+        );
+    }
 }
