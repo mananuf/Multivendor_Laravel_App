315c989ef98e0118ce637cf0af4f9c7efc549539
mananuf <bmananaf@gmail.com>
2022-11-18 23:42:10 +0100

    adds warning message

--- resources/views/components/messages.blade.php
+++ resources/views/components/messages.blade.php
@@ -25,6 +25,15 @@
 </div>
 @endif
 
+@if (session('warning'))
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>warning!</strong> {{session('warning')}}
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+@endif
+
 @if ($errors->any())
     @foreach ($errors->all() as $error)
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
